968132e83c7a6a78edd2426c1e46f4f3b5a40000
Yevhen Hurbych <eugene.gurbych@gmail.com>
2024-05-17 11:50:54 +0100

    JS Authentication 3

--- src/route/auth.js
+++ src/route/auth.js
@@ -6,6 +6,7 @@ const router = express.Router()
 // ================================================================
 
 const { User } = require('../class/user')
+const { Confirm } = require('../class/confirm')
 
 User.create({
   email: 'test@i.com',
@@ -68,6 +69,14 @@ router.post('/signup', function (req, res) {
   }
 
   try {
+    const user = User.getByEmail(email)
+
+    if (user) {
+      return res.status(400).json({
+        message:
+          'Помилка. Користувач з таким email вже існує',
+      })
+    }
     User.create({ email, password, role })
 
     return res.status(200).json({
@@ -79,6 +88,105 @@ router.post('/signup', function (req, res) {
     })
   }
 })
+
+// =======================================================
+
+// Відновлення паролю
+router.get('/recovery', function (req, res) {
+  res.render('recovery', {
+    name: 'recovery',
+    component: ['back-button', 'field'],
+    title: 'Recovery page',
+    data: {},
+  })
+})
+
+router.post('/recovery', function (req, res) {
+  const { email } = req.body
+
+  console.log(email)
+
+  if (!email) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля відсутні",
+    })
+  }
+
+  try {
+    const user = User.getByEmail(email)
+
+    if (!user) {
+      return res.status(400).json({
+        message: 'Користувач з таким email не існує',
+      })
+    }
+
+    Confirm.create(email)
+
+    return res.status(200).json({
+      message: 'Код для відновлення паролю відправлено',
+    })
+  } catch (e) {
+    return res.status(400).json({
+      message: e.message,
+    })
+  }
+})
+
+router.get('/recovery-confirm', function (req, res) {
+  res.render('recovery-confirm', {
+    name: 'recovery-confirm',
+    component: ['back-button', 'field', 'field-password'],
+    title: 'Recovery confirm page',
+    data: {},
+  })
+})
+
+router.post('/recovery-confirm', function (req, res) {
+  const { password, code } = req.body
+
+  console.log(password, code)
+
+  if (!code || !password) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля відсутні",
+    })
+  }
+
+  try {
+    console.log('------------start----------')
+    const email = Confirm.getData(Number(code))
+    console.log('email: ' + email)
+    if (!email) {
+      return res.status(400).json({
+        message: 'Код не існує',
+      })
+    }
+
+    const user = User.getByEmail(email)
+
+    if (!user) {
+      return res.status(400).json({
+        message: 'Користувач не існує',
+      })
+    }
+
+    console.log('test1: ' + user)
+    user.password = password
+    console.log('test2: ' + user)
+
+    return res.status(200).json({
+      message: 'Пароль змінено',
+    })
+  } catch (e) {
+    console.log(e)
+    return res.status(400).json({
+      message: 'Помилка: ' + e.message,
+    })
+  }
+})
+
+// =======================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
